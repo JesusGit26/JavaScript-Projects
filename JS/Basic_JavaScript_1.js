a663d4bdff5adb676a3daabceb5a1678d125dbbd
Jesus S <53194674+JesusGit26@users.noreply.github.com>
2019-08-21 16:12:33 -0700

    Add files via upload

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+
+
+var X = "This is a String";
+
+alert(X);
+
+document.write("\"Let's get this bread fam!"
++ " Get that grip.\""); 
+
+var city = " RIP CITY" + " BABY!", where = " Portland,", when = " Tonigh!";
+var city = city.fontcolor("red");
+var where = where.fontcolor("red");
+var when = when.fontcolor("red");
+document.write(where, when, city);
+
+document.write(5 + 5);
---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,15 @@
+var city = " RIP CITY" + " BABY!", where = " Portland,", when = " Tonigh!";
+var city = city.fontcolor("red");
+var where = where.fontcolor("red");
+var when = when.fontcolor("red");
+document.write(where, when, city);
+
+
+function My_First_Function() {
+    var String = "Hello, World!";
+    var result = String.fontcolor("red");
+    document.getElementById("World").innerHTML =
+     result;
+}
+
+
