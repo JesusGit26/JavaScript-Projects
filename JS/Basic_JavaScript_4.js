1329942f6b0f32dce2071170c7e989b84aca5e67
Jesus S <53194674+JesusGit26@users.noreply.github.com>
2019-08-22 11:42:38 -0700

    Add files via upload

---
+++ JS/Basic_JavaScript_4.js
@@ -0,0 +1,11 @@
+function my_Dict() {
+    var Car = {
+        Body: "Pick-up",
+        Color: "Matte FrostWhite",
+        Make: "Ford",
+        Model: "Raptor",
+        Wheels: 4,
+    };
+    delete.Model;
+    document.getElementById("Dictionary").innerHTML = Car.Model;
+}
